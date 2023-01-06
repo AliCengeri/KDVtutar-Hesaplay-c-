4615e9b2d6acf08539590567bb78640b484f2f51
AliCengeri <120107219+AliCengeri@users.noreply.github.com>
2023-01-06 09:59:51 +0300

    Add files via upload

---
+++ KDV Tutari Hesaplayan Program/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücreti giriniz : " );
+        double ucret;
+        ucret = input.nextDouble();
+        boolean dusuk = (ucret<=1000);
+        double kdvo = (dusuk) ? 0.18 : 0.8;
+        double kdvtutari = ucret * kdvo;
+        double kdvli = ucret + kdvtutari;
+        System.out.println("KDV Tutarı :" + kdvtutari);
+        System.out.println("KDV'li Tutar :" + kdvli);
+        System.out.println("KDV oranı :" + kdvo);
+        System.out.println("KDV'siz Tutar :" + ucret);
+    }
+}
